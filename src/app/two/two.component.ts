03bae8068d7ea001a358192a9c06a25c8641a6f8
CamiloDP96 <cadp.96@outlook.com>
2024-02-09 11:42:54 -0500

    lab complete

---
+++ src/app/two/two.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ShowService } from '../show.service';
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.css']
+})
+export class TwoComponent {
+
+  constructor(private showService: ShowService) { }
+
+  get Items() {
+    return this.showService.items;
+  }
+}
